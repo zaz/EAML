f01494095aaa17c22eb44c37ec464e793f1a8923
Zaz Brown <zazbrown@zazbrown.com>
2018-12-28 13:24:20 -0500

    remove semicolons
    JavaScript does not require semicolons

--- eaml.js
+++ eaml.js
@@ -1,6 +1,6 @@
-var eaml = new Array();
-var System = new Object();
-var isOpened = false;
+var eaml = new Array()
+var System = new Object()
+var isOpened = false
 var api = {
 	tag: {
 		Button:"/eaml/tag/Button.js",
@@ -8,39 +8,39 @@ var api = {
 	feature: {
 		Window:"/eaml/feature/Window.js",
 	},
-};
-eaml.Console = document.createElement("iframe");
-eaml.Console.src = "/eaml/Console.html";
+}
+eaml.Console = document.createElement("iframe")
+eaml.Console.src = "/eaml/Console.html"
 eaml.isDebug = function(bool) {
 	if (bool = true) {
 		
 	} else {
 		
 	}
-};
+}
 eaml.uses = function(scriptUrl) {
-	var head = document.getElementsByTagName("head")[0];
-	script = document.createElement('script');
-	script.type = 'text/javascript';
-	script.src = scriptUrl;
+	var head = document.getElementsByTagName("head")[0]
+	script = document.createElement('script')
+	script.type = 'text/javascript'
+	script.src = scriptUrl
 	script.onload = function() {
 		if (isOpened == false) {
-			main();
-			isOpened = true;
+			main()
+			isOpened = true
 		} else {
-			isOpened = true;
+			isOpened = true
 		}
-	};
-	script.onerror = function(e) { alert("failed: " + JSON.stringify(e)); };
-	head.appendChild(script);
-};
+	}
+	script.onerror = function(e) { alert("failed: " + JSON.stringify(e)) }
+	head.appendChild(script)
+}
 
 (function() {
-	var oldLog = console.log;
+	var oldLog = console.log
 	console.log = function(msg) {
 		if (isDebug(true)) {
-			document.write(msg);
+			document.write(msg)
 		}
-		oldLog.apply(console,arguments);
-	};
-});
+		oldLog.apply(console,arguments)
+	}
+})
